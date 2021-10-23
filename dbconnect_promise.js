2f37c3f71892fff2352d8fd118bc08fb9bb7c6d3
surajit-ai <sdas49610@gmail.com>
2021-10-23 17:10:21 +0530

    connection string change

--- dbconnect_promise.js
+++ dbconnect_promise.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-const url = 'mongodb://localhost:27017/Project';
+// const url = 'mongodb://localhost:27017/Project';
+const url = 'mongodb+srv://SurajitD:SurajitD@cluster0.ynsbr.mongodb.net/react?retryWrites=true&w=majority';
+
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('NODEJS TO MongoDB Connection ESTABLISH.....');
